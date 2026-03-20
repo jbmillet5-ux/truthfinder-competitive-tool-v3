b479cec1dfe84731969648e0ad74b2698f062301
jbmillet5-ux <jb.millet5@gmail.com>
2026-03-20 13:58:32 -0700

    Delete cf-live directory

--- cf-live/functions/export.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestPost() {
-  return new Response("category,value\nsample,test", {
-    headers: {
-      "Content-Type":"text/csv",
-      "Content-Disposition": 'attachment; filename="truthfinder_action_plan.csv"'
-    }
-  });
-}
